2656f0e663d95fda47764ba5ad5ced58c49a2a28
Heshan Padmasiri <hpheshan@gmail.com>
2018-04-24 08:44:53 +0530

    Fixed typos in the last commit

--- src/app/enter-results/enter-results.component.ts
+++ src/app/enter-results/enter-results.component.ts
@@ -16,20 +16,20 @@ export class EnterResultsComponent implements OnInit {
   currentResult = [];
   registeredStudents = [];
   currentModule: string;
-//used to inject things like services when component is being constructed for the further use
+// used to inject things like services when component is being constructed for the further use
   constructor(private userService: UserServicesService,
               private moduleService: ModuleService,
               private flashMessageService: FlashMessagesService) {
   }
-//finished initialising the component
+// finished initialising the component
   ngOnInit() {
-    //executes the observable
+    // executes the observable
     this.userService.getAdminModules().subscribe(message => {
       if (message.success) {
         this.adminModules = message.msg;
         if (this.adminModules.length > 0) {
           this.currentModule = this.adminModules[0];
-          //execute the observable
+          // execute the observable
           this.moduleData = this.moduleService.getModuleData(this.adminModules[0]).subscribe(next  => {
             if (next.success) {
               this.moduleData = next.msg;
